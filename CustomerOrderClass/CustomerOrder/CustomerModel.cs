c686cc2b7debda773b7b62fb9eaeae708f9daa5a
shikomatlala <52504166+shikomatlala@users.noreply.github.com>
2021-07-20 12:04:46 +0200

    Add files via upload

---
+++ CustomerOrderClass/CustomerOrder/CustomerModel.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerOrder
+{
+    class CustomerModel
+    {
+        private int customerID;
+        private string name;
+        private string surname;
+        private string emailAddress;
+        private string phoneNumber;
+        public int age { get; set; }
+
+
+        //GETTERS & SETTERS
+        public string PhoneNumber
+        {
+            get
+            {
+                return phoneNumber;
+            }
+            set
+            {
+                //Ensure that the phone numbers are only numaric values
+                int length = value.Length;
+                bool isNumeric = true;
+                for(int x =0; x < length;x++)
+                {
+                    if (!isNum(value[x]))
+                    {
+                        isNumeric = false;
+                    }
+                }
+
+                if (isNumeric)
+                    phoneNumber = value;
+                else
+                    phoneNumber = "NULL";
+            }
+        }
+        public string EmailAddress
+        {
+            get
+            {
+                return emailAddress;
+            }
+            set
+            {
+                emailAddress = value;
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                bool snameGood = true;
+                if (value.Length > 0)
+                {
+                    for (int x = 0; x < value.Length; x++)
+                    {
+                        if (!isAlpha(value[x]))
+                        {
+                            snameGood = false;
+                            Console.WriteLine("Name has contain alphabets only");
+                            x = value.Length + 1;
+                        }
+
+                    }
+                    if (snameGood)
+                    {
+                        //Set the firstletter to uppercase
+
+                        string firstLetter = value[0].ToString();
+                        firstLetter.ToUpper();
+                        value = value.Substring(0, 1).ToUpper() + value.Substring(1);
+                        surname = value;
+                        Console.WriteLine(surname);
+
+
+
+                    }
+                }
+                else
+                    Console.WriteLine("Invalid Surname");
+            }
+        }
+        public int CustomerID //Set get customeriD
+        {
+            get
+            {
+                return customerID;
+            }
+            set
+            {
+                try
+                {
+                    customerID = value;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Cannot convert " + value + " to int");
+                }
+                 
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name; 
+            }
+            set
+            {
+                //Make sure that the value that you have entered is an alphabet only
+                bool nameGood = true;
+                if (value.Length > 0)
+                {
+                    for (int x = 0; x < value.Length; x++)
+                    {
+                        if (!isAlpha(value[x]))
+                        {
+                            nameGood = false;
+                            Console.WriteLine("Name has contain alphabets only");
+                            x = value.Length + 1;
+                        }
+           
+                    }
+                    if (nameGood)
+                    {
+                        //Set the firstletter to uppercase
+                
+                        string firstLetter = value[0].ToString();
+                        firstLetter.ToUpper();
+                        value = value.Substring(0, 1).ToUpper() + value.Substring(1);
+                        name = value;
+                        Console.WriteLine(name);
+                        
+
+
+                    }
+                }
+                else
+                    Console.WriteLine("Invalid Name");
+            }
+        }
+
+
+        static bool isAlpha(char x)
+        {
+            bool alphaFound = true;
+            for (int y = 97; y <= 122; y++)
+            {
+                if (Convert.ToInt32(x) == y)
+                {
+                    //Console.WriteLine(y);
+                    alphaFound = true;
+                    y = 130;
+                }
+                else
+                    alphaFound = false;
+            }
+            return alphaFound;
+        }
+        static bool isNum(char x)
+        {
+            bool numFound = true;
+            for(int y = 48; y <= 57; y++)
+            {
+                if (Convert.ToInt32(x) != y)
+                {
+                    numFound = false;
+                    y = 58;
+                }
+            }
+            return numFound;
+
+        }
+    }
+}
---
+++ CustomerOrderClass/CustomerOrder/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerOrder
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Let us check if the value is numeric
+
+            CustomerModel x1 = new CustomerModel();
+            Console.Write("CUSTOMER ID :  ");
+            try
+            {
+                x1.CustomerID = Convert.ToInt32(Console.ReadLine());
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Cannot convert the format to int value has been set automatically");
+                x1.CustomerID = 0;
+            }
+            Console.Write("NAME :  ");
+            x1.Name = Console.ReadLine();
+            Console.Write("SURNAME :  ");
+            x1.Surname = Console.ReadLine();
+            Console.Write("EMAIL ADDRESS :  ");
+            x1.EmailAddress = Console.ReadLine();
+            Console.Write("PHONE NUMBER :  ");
+            x1.PhoneNumber = Console.ReadLine();
+
+            Console.WriteLine(x1.PhoneNumber);
+            Console.ReadLine();
+
+            Console.ReadLine();
+        } 
+
+    }
+}
+
+//char x = 'a';
+////char y = '0';
+//char z = '9';
+
+//bool numFound = true;
+
+//if(numFound)
+//{
+//    Console.WriteLine(x + " is a number");
+//}
+//else
+//{
+
+//    Console.WriteLine(x + " is not a number");
+//}
+
+//CustomerModel x1 = new CustomerModel();
+//x1.CustomerID = 1;
+//            x1.Name = "shiko";
+//            x1.Surname = "Matlala";
+//            x1.EmailAddress = "shikomatlala@gmail.com";
+//            x1.PhoneNumber = "0129093322";
+
+//            Console.WriteLine(x1.Name + "\n" + x1.Surname + "\nCustomerID " + x1.CustomerID + "\n" + x1.EmailAddress + "\n" + x1.PhoneNumber);
+
+//            Console.ReadLine();
+
+////Create our object
+//CustomerModel people = new CustomerModel();
+////people.customerID = 1;
+////people.name = "Shiko";
+////people.surname = "Matlala";
+////people.emailAddress = "shikomatlala@gmail.com";
+
+
+//CustomerModel people2 = new CustomerModel(2, "Bonolo", "Shilaluke", "bonololuke@gmail.com", "012");
+////CustomerModel people3 = new CustomerModel();
+////people2();
+
+
+////Print the customer that I just created
+////Console.WriteLine(people.emailAddress +  "  " + people.name + " " + people.surname);
+//Console.WriteLine();
+//Console.WriteLine(people2.customerID.ToString() + " \n" + people2.name + " \n" + people2.surname + " \n" + people2.emailAddress + " \n" + people2.phoneNumber);
+
